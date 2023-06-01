537d48329bcf65cdfcff457f8bb2a5c20a274809
Seval Senturk <seval.senturk@tgs.aero>
2023-06-01 10:33:32 +0300

    kursların düzenlemesi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Courses from "./Courses";
+import Loading from "./Loading";
 
 function App() {
-  return <div className="App">Deneme</div>;
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const deleteCourse = (id) => {
+    const afterDeletedCourses = courses.filter((course) => course.id !== id);
+    setCourses(afterDeletedCourses);
+  };
+
+  const fetchCourses = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("http://localhost:3000/courses");
+      setCourses(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+
+    //debugger;
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+  return (
+    <div className="App">
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {courses.length === 0 ? (
+            <div className="refreshDiv">
+              <h2>Kursların hepsini sildin...!</h2>
+              <button className="refreshBtn" onClick={()=>{
+                fetchCourses();
+              }}>Yenile</button>
+            </div>
+          ) : (
+            <Courses courses={courses} removeCourse={deleteCourse} />
+          )}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
